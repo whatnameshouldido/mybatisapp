2c2fd94025b8cabc60447ea2337d68897106472d
whatnameshouldido <pobyjun@naver.com>
2024-07-25 14:20:30 +0900

    add debugging
    add debugging

--- mybatisapp/src/main/java/com/studymavernspringboot/mybatisapp/category/CategoryWebController.java
+++ mybatisapp/src/main/java/com/studymavernspringboot/mybatisapp/category/CategoryWebController.java
@@ -27,6 +27,8 @@ public class CategoryWebController {
             model.addAttribute("allList", allList);
         } catch (Exception ex) {
             log.error(ex.toString());
+            model.addAttribute("error_message", "오류가 발생했습니다. 관리자에게 문의하세요.");
+            return "error/error_save";
         }
         return "oldhtml/category_old";  // resources/templates 폴더안의 화면파일
     }
@@ -36,13 +38,13 @@ public class CategoryWebController {
         try {
             if ( dto == null || dto.getName() == null || dto.getName().isEmpty() ) {
                 model.addAttribute("error_message", "이름이 비었습니다.");
-                return "error/error_insert";  // 브라우저 주소를 redirect 한다.
+                return "error/error_bad";  // 브라우저 주소를 redirect 한다.
             }
             this.categoryService.insert(dto);
         } catch (Exception ex) {
             log.error(ex.toString());
             model.addAttribute("error_message", dto.getName() + " 중복입니다.");
-            return "error/error_insert";  // resources/templates 폴더안의 화면파일
+            return "error/error_save";  // resources/templates 폴더안의 화면파일
         }
         return "redirect:category_old";  // 브라우저 주소를 redirect 한다.
     }
@@ -50,12 +52,16 @@ public class CategoryWebController {
     @GetMapping("/oldhtml/category_old_view")    // 브라우저의 URL 주소
     public String categoryOldView(Model model, @RequestParam Long id) {
         try {
+            if ( id == null || id  <= 0) {
+                model.addAttribute("error_message", "ID는 1보다 커야 합니다.");
+                return "error/error_bad";
+            }
             ICategory find = this.categoryService.findById(id);
-            if ( id == null || id) {
-                model.addAttribute("NothingId", find);
+            if(find == null) {
+                model.addAttribute("error_message", id + "데이터가 없습니다.");
                 return "error/error_find";
             }
-            model.addAttribute("allList", find);
+            model.addAttribute("KJSData", find);
         } catch (Exception ex) {
             log.error(ex.toString());
         }
@@ -66,16 +72,18 @@ public class CategoryWebController {
     public String categoryOldUpdate(Model model, @ModelAttribute CategoryDto categoryDto) {
         try {
             if (categoryDto == null || categoryDto.getId() <= 0 || categoryDto.getName().isEmpty()) {
-                return "redirect:category:old";
+                return "error/error_bad";
             }
             ICategory find = this.categoryService.findById(categoryDto.getId());
             if ( find == null ) {
-                model.addAttribute("NothingId", categoryDto.getId());
+                model.addAttribute("error_message", categoryDto.getId() + " 데이터가 없습니다.");
                 return "error/error_find";
             }
             this.categoryService.update(categoryDto.getId(), categoryDto);
         }catch (Exception ex) {
             log.error(ex.toString());
+            model.addAttribute("error_message", categoryDto.getName() + " 중복입니다.");
+            return "error/error_save";  // resources/templates 폴더안의 화면파일
         }
         return "redirect:category_old";
     }
@@ -84,16 +92,19 @@ public class CategoryWebController {
     public String categoryDelete(Model model, @RequestParam Long id) {
         try {
             if (id == null || id <= 0) {
-                return "redirect:category_old";
+                model.addAttribute("error_message", "id는 1보다 커야 합니다.");
+                return "error/error_bad";
             }
             ICategory find = this.categoryService.findById(id);
             if ( find == null ) {
-                model.addAttribute("NothingId", find);
+                model.addAttribute("error_message", id + " 데이터가 없습니다.");
                 return "error/error_find";
             }
             this.categoryService.remove(id);
         }catch (Exception ex) {
             log.error(ex.toString());
+            model.addAttribute("error_message", "서버 에러입니다. 관리자에게 문의 하세요.");
+            return "error/error_save";  // resources/templates 폴더안의 화면파일
         }
         return "redirect:category_old";
     }
